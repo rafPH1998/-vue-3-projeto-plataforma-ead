eba5615ba3d223f87350599e6f6ee16f57cb1a8e
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-10-10 15:37:59 -0300

    View de supports

---
+++ src/views/modules/components/SupportsView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="comments">
+        <div class="header">
+            <span class="title">Dúvidas</span>
+            <button class="btn primary">
+                <i class="fas fa-plus"></i>
+                Enviar nova dúvida
+            </button>
+        </div>
+        
+        <SupportsGlobal/>
+    </div>
+</template>
+
+<script>
+import SupportsGlobal from '@/components/SupportsGlobal.vue';
+export default {
+   name: 'SupportsView',
+   components: {
+    SupportsGlobal
+   }
+}
+</script>
