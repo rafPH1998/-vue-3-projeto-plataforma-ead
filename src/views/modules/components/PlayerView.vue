648aa66c61397ae83d366a6594f15207e26a9b5e
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-10-10 15:38:23 -0300

    component de vídeo, aonde ficará toda estrutura

---
+++ src/views/modules/components/PlayerView.vue
@@ -0,0 +1,22 @@
+<template>
+     <div class="training">
+        <div class="card bg-laravel">
+            <span class="icon">
+                <img src="images/icons/laravel.svg" alt="">
+            </span>
+            <span class="title">Aula 01 - LaraFood</span>
+            <router-link 
+                :to="{name: 'campus.home'}" class="btn laravel">
+                <i class="fas fa-chevron-left"></i>
+                Voltar
+            </router-link>
+        </div>
+        <iframe width="100%" height="auto" src="https://www.youtube.com/embed/KuzeyRr74xM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PlayerView',
+}
+</script>
