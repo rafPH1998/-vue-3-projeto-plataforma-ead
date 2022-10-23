d3eea33da64db86e4cfa115af58cd3fc2f69e77a
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-10-23 20:14:29 -0300

    camada AuthService para processo de autenticação

--- src/services/AuthServices.js
+++ src/services/AuthServices.js
@@ -1,10 +1,18 @@
-import { URL_API } from "@/configs";
-import axios from "axios";
+import { TOKEN_NAME } from "@/configs";
+import BaseService from "./BaseService";
 
-export default class AuthServices {
+export default class AuthServices extends BaseService {
 
     static async auth (params)
     {
-        return axios.post(`${URL_API}/auth`, params)
+        return new Promise((resolve, reject) => {
+            this.request()
+                .post('/auth', params)
+                .then(response => {
+                    localStorage.setItem(TOKEN_NAME, response.data.token)
+                    resolve(response)
+                })
+                .catch(error => reject(error.response))
+        })
     }
 }
